dea23bb5bbe338c4af20a64a5f998dea3dbf584d
Owain The Dev <128409488+OwainPegg@users.noreply.github.com>
2023-03-20 17:17:29 +0000

    RotateObject

---
+++ RotateObject.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+public class RotateObject : MonoBehaviour
+{
+    public Vector3 RotateAmount;  // degrees per second to rotate in each axis. Set in inspector.
+    public int CutSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotateAmount * Time.deltaTime / CutSpeed);
+    }
+}
+
+// By Owain The Dev
